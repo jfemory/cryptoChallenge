c4d8015c94353b4bddfe88d29a351de52ddf1ef8
jfemory <jfemory@gmail.com>
2019-03-10 09:11:44 -0500

    Solved 1.4, add outputData struct,

--- 1.3-singleByteXOR/1.3-singleByteXOR.go
+++ 1.3-singleByteXOR/1.3-singleByteXOR.go
@@ -1,6 +1,6 @@
 /*Set 1 Exercise 2: Single-Byte XOR.
 Expected Input: "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-Expected Output: ""
+Expected Output: "Cooking MC's like a pound of bacon"
 */
 
 package main
