1b533a3719713da2bc5a22e88b050f6bfa80bf9d
rev <jfemory@gmail.com>
2019-03-10 06:34:50 -0500

    Added 1.3 tests.

---
+++ 1.3-singleByteXOR/1.3-singleByteXOR.go
@@ -0,0 +1,12 @@
+/*Set 1 Exercise 2: Single-Byte XOR.
+Expected Input: "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+Expected Output: ""
+*/
+
+package main
+
+// c for crypto
+
+func main() {
+
+}
---
+++ lib/score.go
@@ -0,0 +1,37 @@
+package lib
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
+
+type score interface {
+	score() float64
+}
+
+func ScoreString(s score) float64 {
+	out := s.score()
+	return out
+}
+
+func (str Hexstr) score() float64 {
+	decoded, _ := hex.DecodeString(string(str))
+	out := scoreBytes(decoded)
+	return out
+}
+
+func (str Base64str) score() float64 {
+	decoded, _ := base64.StdEncoding.DecodeString(string(str))
+	out := scoreBytes(decoded)
+	return out
+}
+
+func scoreBytes(b []byte) float64 {
+	score := 0
+	for _, v := range b {
+		if (v > 64 && v < 91) || (v > 96 && v < 127) {
+			score++
+		}
+	}
+	return (float64(score) / float64(len(b)))
+}
